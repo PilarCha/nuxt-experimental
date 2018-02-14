822cb94fc3c9067e74140e2d35d194d6b223bb4c
Pilar Chavez <pchavez92@gmail.com>
2018-02-14 13:19:53 -0800

    Incorporated async data so no need for store

--- client/middleware/search.js
+++ client/middleware/search.js
@@ -1,3 +1,10 @@
-export default function(context) {
-  context.params.id = 'Changed'
-}
+// import axios from 'axios'
+// export default function( {params, store}) {
+//
+//   return axios.get(`https://itunes.apple.com/search?term=${params.id}&entityalbum`)
+//     .then((response) => {
+//       store.commit('add', response.data.results);
+//     })
+// }
+
+//uncomment to incorporate a store otherwise we just used async data
--- client/pages/results/_id.vue
+++ client/pages/results/_id.vue
@@ -1,12 +1,23 @@
 <template lang="html">
   <div class="idValue">
     <h1>Hello from the route of {{$route.params.id}}</h1>
+    {{albumData}}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  middleware: 'search'
+  data () {
+    albumData: []
+  },
+  asyncData({ params }) {
+    return axios.get(`https://itunes.apple.com/search?term=${params.id}&entityalbum`)
+      .then((response) => {
+        return {albumData: response.data.results}
+      })
+  }
+  // middleware: 'search' //want to use a store? go ahead and uncomment this 
 }
 </script>
 
